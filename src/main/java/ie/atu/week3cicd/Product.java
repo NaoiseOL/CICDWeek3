7e67b57207a69c60ccd13613c40c6673b31a1272
NaoiseOL <naoiseol@gmail.com>
2024-10-08 09:51:21 +0100

    Created New project with Product and Product Controller functions

---
+++ src/main/java/ie/atu/week3cicd/Product.java
@@ -0,0 +1,14 @@
+package ie.atu.week3cicd;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String id;
+    private String name;
+    private String category;
+    private double price;
+}
